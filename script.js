6fee7785a59605a5d1960c8e28c676f64e2dbff4
Theo-06130 <theo.cerkownik@ynov.com>
2023-08-25 12:56:21 +0200

    récupération API avec value pré repmpli

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("test");
+
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+};
+
+fetch("https://api.spoonacular.com/recipes/complexSearch?query=pasta&cuisine=italian&apiKey=f528c4f6716d471b9f7c6b06c018182d", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
