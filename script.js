684a7b76cd089a97c16ebf54470a9b2535a72565
Theo-06130 <theo.cerkownik@ynov.com>
2023-08-26 12:28:30 +0200

    permet de changer le filte du pays d'origine avec un select

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 console.log("test");
+/*let cuisine_info= document.getElementById("country_cuisine").value*/
+
 
 var requestOptions = {
     method: 'GET',
     redirect: 'follow'
 };
 
-fetch("https://api.spoonacular.com/recipes/complexSearch?query=pasta&cuisine=italian&apiKey=f528c4f6716d471b9f7c6b06c018182d", requestOptions)
-    .then(response => response.json())
-    .then(result => console.log(result.results[0].title))
-    .catch(error => console.log('error', error));
+document.getElementById('country_cuisine').addEventListener('change', function () {
+    const cuisine_info = this.value;
+
+    fetch("https://api.spoonacular.com/recipes/complexSearch?cuisine="+cuisine_info+"&apiKey=f528c4f6716d471b9f7c6b06c018182d", requestOptions)
+        .then(response => response.json())
+        .then(result => console.log(result.results[0].title))
+        .catch(error => console.log('error', error));
+
+});
